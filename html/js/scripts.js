52e34839347434dfe83225294b9537afad23e49b
crazyyy <crazyyy@gmail.com>
2015-06-04 12:44:20 +0300

    ready

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   var passField = $('#pass'),
-    passFieldTwo = $('#pass-two');
+    passFieldTwo = $('#pass-two'),
+    fieldThree = $('#random-number'),
+    minNumber = 36,
+    maxNumber = 60;
+
+
   $('.generator').pGenerator({
     'bind': 'click',
     'passwordElement': ['#pass', '#pass-two'],
@@ -11,9 +16,19 @@ $(document).ready(function () {
     'numbers':   true,
     'specialChars': false,
     'onPasswordGenerated': function (generatedPassword) {
-      console.log('My new generated password is ' + generatedPassword);
       passField.val(generatedPassword);
       passFieldTwo.val(generatedPassword);
+
+      randomNumberFromRange(minNumber / 2, maxNumber / 2);
+      function randomNumberFromRange(min, max) {
+        var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        fieldThree.val(randomNumber * 2);
+      }
+
+
+
+
+
     }
   });
 });
